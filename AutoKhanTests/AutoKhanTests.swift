d78ce560d67820d01abba78950c2c78a7ec0ede8
Adam Kirk <atomkirk@gmail.com>
2015-10-26 19:14:58 -0600

    xcode proj

---
+++ AutoKhanTests/AutoKhanTests.swift
@@ -0,0 +1,36 @@
+//
+//  AutoKhanTests.swift
+//  AutoKhanTests
+//
+//  Created by Adam Kirk on 10/26/15.
+//  Copyright © 2015 Adam Kirk. All rights reserved.
+//
+
+import XCTest
+@testable import AutoKhan
+
+class AutoKhanTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
